a2dfe79d51965e34e2ec0034d9fc0af683f24802
slavablack <veaceslav.ciornii@magenta.de>
2022-08-31 11:01:37 +0200

    basic backend created

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to our page!" });
+});
+app.listen(process.env.PORT, () => {
+  console.log("app listen to port " + process.env.PORT);
+});
